a290d849d9957db38439d73fb2f0253a2190cc60
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-03-26 14:05:06 -0400

    initializing

---
+++ mailer/form_setting.php
@@ -0,0 +1,9 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Luique - CV Resume Template'"; //letter subject
+	$to = 'hovigkalayjian@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
+
